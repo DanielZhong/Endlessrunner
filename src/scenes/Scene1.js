ae9ba71c73694df499ce28a7f1c3787205d06645
“PennMak” <tsmak@ucsc.edu>
2022-10-10 10:22:25 -0700

    Update Scene1.js

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -14,7 +14,7 @@ class Scene1 extends Phaser.Scene {
         this.load.image('vblock2', './assets/verticalblock3.png');
         this.load.audio('jump', './assets/jump.wav');
         this.load.audio('dead', './assets/dead.wav');
-        this.load.spritesheet('character', './assets/texture_atlas.png', {frameWidth: 85, frameHeight: 85});
+        this.load.spritesheet('character', './assets/texture_atlas.png', {frameWidth: 270, frameHeight: 270});
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
     }
